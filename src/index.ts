91cd2d38457e490f38766d8289add2c0cd029eb4
Jack Le Hamster <jacklehamster@gmail.com>
2023-06-07 01:41:41 -0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 const yaml = require('js-yaml');
 
 export const yamlFetch = async (input: RequestInfo | string, init?: RequestInit): Promise<Response & {yaml():Promise<any>}> => {
-  const response = await global.fetch(input, init);
-  return {
-      ...response,
-      yaml: async () => yaml.load(await response.text()),
-  }
+  const response = await global.fetch(input, init) as Response & {yaml():Promise<any>};
+  response.yaml = async () => yaml.load(await response.text());
+  return response;
 }
