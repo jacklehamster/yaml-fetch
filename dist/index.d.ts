47ae6b989f5c90a075918a7f3a7984c93d103721
Jack Le Hamster <jacklehamster@gmail.com>
2023-06-15 02:01:43 -0700

    ESM

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,4 @@
-declare const yaml: any;
+import yaml from "js-yaml";
 export declare const yamlFetch: (input: RequestInfo | string, init?: RequestInit | undefined) => Promise<Response & {
     yaml(): Promise<any>;
 }>;
-export {};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-const yaml = require('js-yaml');
+import yaml from "js-yaml";
 
 export const yamlFetch = async (input: RequestInfo | string, init?: RequestInit): Promise<Response & {yaml():Promise<any>}> => {
   const response = await global.fetch(input, init) as Response & {yaml():Promise<any>};
